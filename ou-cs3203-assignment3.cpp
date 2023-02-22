dd33ef2ff2fe6ba2e056b71cff95ef5ea43ec47b
Crisco8828 <crisco.dalton@yahoo.com>
2023-02-21 21:41:07 -0600

    Part 2 - Product of the Array
    Multiply the 3 numbers in the array to get a product.

--- ou-cs3203-assignment3.cpp
+++ ou-cs3203-assignment3.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 // Function Prototype
 int sumArray(int x, int y, int z);
-
+int productArray(int x, int y, int z);
 
 int sumArray(int x, int y, int z){
 
@@ -18,4 +18,21 @@ int sumArray(int x, int y, int z){
 
     return sum;
 
+}
+
+
+// Step 2 - Product of Array
+int productArray(int x, int y, int z){
+
+
+    int product = 1;
+    int array[3]={x,y,x};
+
+    for(int i = 0; i< 3;i++){
+
+        product *= array[i];
+
+    }
+
+    return product;
 }
