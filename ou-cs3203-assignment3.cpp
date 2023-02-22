1c35051f2688278d4b29740a2308c0d20622df01
Crisco8828 <crisco.dalton@yahoo.com>
2023-02-21 21:39:57 -0600

    Part 1 - Sum of the Array
    Add the 3 numbers in the array.

--- ou-cs3203-assignment3.cpp
+++ ou-cs3203-assignment3.cpp
@@ -1,11 +1,21 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+// Function Prototype
+int sumArray(int x, int y, int z);
 
-    cout << "\n\nHello World\n\n";
 
-    return 0;
+int sumArray(int x, int y, int z){
 
+    int sum = 0;
+    int array[3] = {x,y,z};
+
+    for(int i = 0; i < 3; i++){
+
+        sum += array[i];
+
+    }
+
+    return sum;
 
 }
